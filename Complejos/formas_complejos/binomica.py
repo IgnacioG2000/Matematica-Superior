523c3919a057580cf4bd605fcca07807f54677c9
IgnacioG2000 <nachog.2000@hotmail.com>
2022-08-22 14:36:54 -0300

    Forma polar hecha

---
+++ Complejos/formas_complejos/binomica.py
@@ -0,0 +1,2 @@
+def forma_binomica(parte_real, parte_imaginaria):
+    return complex(parte_real, parte_imaginaria)
--- Complejos/main.py
+++ Complejos/main.py
@@ -1,9 +1,17 @@
+from Complejos.formas_complejos.binomica import forma_binomica
+from Complejos.formas_complejos.polar import *
 from Complejos.grafico import dibujar_complejos
 
-parte_real = float(input("Digita la parte real del complejo: "))
-parte_imaginaria = float(input("Digita la parte imaginaria del complejo: "))
+parte_real = float(input("Digita la parte real del complejo_binomica: "))
+parte_imaginaria = float(input("Digita la parte imaginaria del complejo_binomica: "))
 
-complejo = complex(parte_real, parte_imaginaria)
+complejo_binomica = forma_binomica(parte_real, parte_imaginaria)
+modulo_complejo = modulo_complejo(parte_real, parte_imaginaria)
+fase_complejo = fase_complejo(parte_real, parte_imaginaria)
+complejo_polar = forma_polar(parte_real, parte_imaginaria)
 
-print(complejo)
-dibujar_complejos(complejo)
+print(f"El complejo en forma binomica es: {complejo_binomica}\n")
+print(f"El modulo del complejo es: {modulo_complejo}")
+print(f"La fase del complejo es: {fase_complejo}")
+print(f"El complejo en forma polar es: {complejo_polar}")
+dibujar_complejos(complejo_binomica)
