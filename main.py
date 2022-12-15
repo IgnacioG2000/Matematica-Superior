de5accf77f5b84f491b88d90757436f0426007d9
IgnacioG2000 <nachog.2000@hotmail.com>
2022-12-15 20:31:58 -0300

    Se linkea bien el css

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -6,6 +6,16 @@ app = Flask(__name__)
 @app.route('/')
 def Index():
     return render_template('index.html')
+
+
+@app.route('/binomica', methods=['POST'])
+def binomica():
+    if request.method == 'POST':
+        parte_real = request.form['parte_real']
+        parte_imaginaria = request.form['parte_imaginaria']
+        print(parte_real)
+        print(parte_imaginaria)
+        return "recibido"
 
 
 if __name__ == '__main__':
