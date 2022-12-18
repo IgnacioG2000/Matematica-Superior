93fb7a5c3ed101db6ceb67282f941709dbdcc031
IgnacioG2000 <nachog.2000@hotmail.com>
2022-12-18 17:52:56 -0300

    Se muestran los 4 modos de los numeros complejos

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request
 
 from Complejos.formas_complejos.binomica import forma_binomica
+from Complejos.formas_complejos.exponencial import forma_exponencial
+from Complejos.formas_complejos.polar import forma_polar
+from Complejos.formas_complejos.trigonometrica import forma_trigonometrica
 
 app = Flask(__name__)
 
@@ -11,16 +14,23 @@ def Index():
 
 
 @app.route('/complejos', methods=['POST', 'GET'])
-def binomica():
+def complejos_con_grafico():
     if request.method == 'POST':
         parte_real = request.form['parteReal']
         parte_imaginaria = request.form['parteImaginaria']
-        complejo_binomica = forma_binomica(float(parte_real), float(parte_imaginaria))
         operacion = request.form['operacion']
         if operacion == 'binomica':
-            print(operacion)
-
-        return render_template('complejos_con_grafico.html', complejo=complejo_binomica)
+            complejo_binomica = forma_binomica(float(parte_real), float(parte_imaginaria))
+            return render_template('complejos_con_grafico.html', complejo=complejo_binomica, operacion=operacion)
+        elif operacion == 'exponencial':
+            complejo_exponencial = forma_exponencial(float(parte_real), float(parte_imaginaria))
+            return render_template('complejos_con_grafico.html', complejo=complejo_exponencial, operacion=operacion)
+        elif operacion == 'trigonometrica':
+            complejo_trigonometrica = forma_trigonometrica(float(parte_real), float(parte_imaginaria))
+            return render_template('complejos_con_grafico.html', complejo=complejo_trigonometrica, operacion=operacion)
+        else:
+            complejo_polar = forma_polar(float(parte_real), float(parte_imaginaria))
+            return render_template('complejos_con_grafico.html', complejo=complejo_polar, operacion=operacion)
     else:
         return render_template('complejos_con_grafico.html')
 
