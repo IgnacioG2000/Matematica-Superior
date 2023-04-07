8b935c0e2cf7d21038d1f53a2c8bbb33331f9a73
IgnacioG2000 <nachog.2000@hotmail.com>
2023-04-07 13:29:43 -0300

    Anda el display de ec diferenciales. Revisar un poco el codigo

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,3 +1,4 @@
+/*
 const operacion = document.getElementById('operacion')
 
 function alertarDivisionPor0(operacion) {
@@ -16,3 +17,45 @@ operacion.addEventListener('click', e=> {
     alertarDivisionPor0(this.operacion.value)
 })
 
+*/
+const orden = document.getElementById('ordenEcuacion')
+
+function mostrarOrdenEcuacionSegunOpcion(ordenEc) {
+    if(ordenEc === '3') {
+        document.getElementById('mostrarResultadoEcDiferencialOrden3').style.display = 'block'
+        document.getElementById('mostrarValoresInicialesEcDiferencialOrden3').style.display = 'block'
+        document.getElementById('mostrarResultadoEcDiferencialOrden2').style.display = 'block'
+        document.getElementById('mostrarValoresInicialesEcDiferencialOrden2').style.display = 'block'
+        document.getElementById('mostrarResultadoEcDiferencialOrden1').style.display = 'block'
+        document.getElementById('mostrarValoresInicialesEcDiferencialOrden1').style.display = 'block'
+        document.getElementById('mostrarFuncionYTerminoIndependiente').style.display = 'block'
+    }
+    else {
+        if(ordenEc === '2') {
+            document.getElementById('mostrarResultadoEcDiferencialOrden3').style.display = 'none'
+            document.getElementById('mostrarValoresInicialesEcDiferencialOrden3').style.display = 'none'
+            document.getElementById('mostrarResultadoEcDiferencialOrden2').style.display = 'block'
+            document.getElementById('mostrarValoresInicialesEcDiferencialOrden2').style.display = 'block'
+            document.getElementById('mostrarResultadoEcDiferencialOrden1').style.display = 'block'
+            document.getElementById('mostrarValoresInicialesEcDiferencialOrden1').style.display = 'block'
+            document.getElementById('mostrarFuncionYTerminoIndependiente').style.display = 'block'
+        }
+        else {
+            if(ordenEc === '1') {
+                document.getElementById('mostrarResultadoEcDiferencialOrden1').style.display = 'block'
+                document.getElementById('mostrarValoresInicialesEcDiferencialOrden1').style.display = 'block'
+                document.getElementById('mostrarFuncionYTerminoIndependiente').style.display = 'block'
+                document.getElementById('mostrarResultadoEcDiferencialOrden2').style.display = 'none'
+                document.getElementById('mostrarValoresInicialesEcDiferencialOrden2').style.display = 'none'
+                document.getElementById('mostrarResultadoEcDiferencialOrden3').style.display = 'none'
+                document.getElementById('mostrarValoresInicialesEcDiferencialOrden3').style.display = 'none'
+            }
+        }
+    }
+
+}
+
+orden.addEventListener('change', e => {
+    e.preventDefault();
+    mostrarOrdenEcuacionSegunOpcion(orden.value)
+})
